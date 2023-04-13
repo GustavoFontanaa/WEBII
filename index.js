5d9528eea274cb8463ae0586d65457adf2b1f039
Gustavo Fontana <gustavoasf77@gmail.com>
2023-04-13 20:14:20 -0300

    implementa tela de tarefas

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
-const express = require('express');
-const exphbs = require('express-handlebars');
-const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
-const session = require('express-session');
-const users = require('./dados/users.json');
-const fs = require('fs');
+const express = require("express");
+const exphbs = require("express-handlebars");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
+const users = require("./dados/users.json");
+const tasks = require("./dados/tasks.json");
+const fs = require("fs");
 
 const app = express();
 
 // Configuração do Handlebars
-app.engine('handlebars', exphbs.engine());
-app.set('view engine', 'handlebars');
+app.engine("handlebars", exphbs.engine());
+app.set("view engine", "handlebars");
 
 // Configuração do Body Parser
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -22,16 +23,16 @@ app.use(cookieParser());
 // Configuração do Express Session
 app.use(
   session({
-    secret: '777@#978terghbdfgpt',
+    secret: "777@#978terghbdfgpt",
     resave: false,
     saveUninitialized: true,
   })
 );
 
 // public path
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-app.post('/login', (req, res) => {
+app.post("/login", (req, res) => {
   const { email, password } = req.body;
 
   // Verificar se o usuário existe no arquivo users.json
@@ -40,52 +41,73 @@ app.post('/login', (req, res) => {
   );
 
   if (user) {
-    res.render('tasks/tasks.handlebars');
+    res.render("tasks/tasks.handlebars");
   } else {
-    res.render('auth/login', { error: 'Credenciais inválidas' });
+    res.render("auth/login", { error: "Credenciais inválidas" });
   }
 });
 
 // Rota para processar o cadastro
-app.post('/register', (req, res) => {
+app.post("/register", (req, res) => {
   const { email, password } = req.body;
-
-  // const usersJSON = fs.readFileSync('users.json');
-  // const users = JSON.parse(usersJSON);
 
   users.users.push({ email, password });
 
   // Verificar se o usuário já existe no arquivo users.json
   const userExists = users.users.find((user) => user.email === email);
 
-  fs.writeFileSync('dados/users.json', JSON.stringify(users));
+  fs.writeFileSync("dados/users.json", JSON.stringify(users));
 
   if (userExists) {
-    res.render('auth/register');
+    res.render("auth/register");
   } else {
     users.users.push({ email, password });
-    res.render('auth/login');
+    res.render("auth/login");
+  }
+});
+
+//Cadastro de Tarefas
+app.post("/register/tasks", (req, res) => {
+  const { name, task, time_finish } = req.body;
+
+  tasks.tasks.push({ name, task, time_finish });
+
+  const taskExists = tasks.tasks.find((tasks) => tasks.name === name);
+
+  fs.writeFileSync("dados/tasks.json", JSON.stringify(tasks));
+
+  if (taskExists) {
+    tasks.tasks.push({ name, task, time_finish });
+    res.render("tasks/registerTasks");
+  } else {
+    res.render("tasks/registerTasks", {
+      error: "Tarefa já existe na base de dados", //ta bugado aqui
+    });
   }
 });
 
 // Rota principal
-app.get('/', (req, res) => {
-  res.render('home/home');
+app.get("/", (req, res) => {
+  res.render("home/home");
 });
 
-app.get('/login', (req, res) => {
-  res.render('auth/login');
+app.get("/login", (req, res) => {
+  res.render("auth/login");
 });
 
-app.get('/register', (req, res) => {
-  res.render('auth/register');
+app.get("/register", (req, res) => {
+  res.render("auth/register");
 });
 
-app.get('/tasks', (req, res) => {
-  res.render('tasks/tasks');
+app.get("/tasks", (req, res) => {
+  res.render("tasks/tasks");
+});
+
+app.get("/register/tasks", (req, res) => {
+  res.render("tasks/registerTasks");
 });
 
 app.listen(3000, () => {
-  console.log('Servidor rodando na porta 3000');
-  console.log('Acesse: http://localhost:3000');
+  console.log("Servidor rodando na porta 3000");
+  console.log("Acesse: http://localhost:3000");
 });
